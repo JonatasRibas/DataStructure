89065231ce7a5681f8666ccf1448800cdcd2bea8
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2023-03-13 12:40:13 -0300

    DataStruct 3.0

---
+++ DataStructure-main/DataStructure-main/js/fila.js
@@ -0,0 +1,25 @@
+var fila = []
+
+const add = document.querySelector('#add')
+const rm = document.querySelector('#rm')
+
+add.addEventListener("click", function(e){
+
+    e.preventDefault()
+    dado = document.querySelector('#numero').value
+    fila.push(dado)
+    console.log(fila)
+    document.querySelector('#resp').innerHTML = fila
+    return false
+})
+
+rm.addEventListener("click", function(e){
+
+    e.preventDefault()
+    dado = document.querySelector('#numero').value
+    fila.pop(dado)
+    console.log(fila)
+    document.querySelector('#resp').innerHTML = fila
+    return false
+})
+
---
+++ DataStructure-main/DataStructure-main/js/script.js
@@ -0,0 +1,14 @@
+//const || Let || 
+
+const bt = document.querySelector("#enviar")
+
+bt.addEventListener("click", function(e){
+
+    //Não deixar atualizar o formulário || Página
+    e.preventDefault()
+    const numero = document.querySelector('#numero')
+    const valor = numero.value
+    console.log(valor)
+    let resposta = document.querySelector("#resp").innerHTML = Number(valor)*2
+})
+
